6942484d944e87d537886e1f1bd469cbb4a7fa7a
RistoFlink <kenssi@gmail.com>
2023-11-13 13:45:08 +0200

    Add basic menu layout and important ASCII art

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,47 @@ use crate::pentry::read_passwords_from_file;
 fn clr() {
     print!("{}[2J", 27 as char);
 }
+
+fn main() {
+    clr();
+    let ascii = r#"
+░░░░░░░░░░░░░░░░░░░░
+░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░
+░█░░░░░░░░▀▄░░░░░░▄░
+█░░▀░░▀░░░░░▀▄▄░░█░█
+█░▄░█▀░▄░░░░░░░▀▀░░█
+█░░▀▀▀▀░░░░░░░░░░░░█
+█░░░░░░░░░░░░░░░░░░█
+█░░░░░░░░░░░░░░░░░░█
+░█░░▄▄░░▄▄▄▄░░▄▄░░█░
+░█░▄▀█░▄▀░░█░▄▀█░▄▀░
+░░▀░░░▀░░░░░▀░░░▀░░░
+    "#;
+    println!("{ascii}");
+    loop {
+        println!("Password manager menu: ");
+        println!("1. Add entry");
+        println!("2. List entries");
+        println!("3. Search");
+        println!("4. Quit");
+
+        let mut choice = String::new();
+        std::io::stdin().read_line(&mut choice).unwrap();
+
+        match choice.trim() {
+            "1" => {
+
+            },
+            "2" => {
+
+            },
+            "3" => {
+
+            },
+            "4" => {
+
+            },
+        }
+
+    }
+}
